3e67eca28b9e2b34be4b4eae813a9dc5666c677f
maeka <maek.alekdawi@gmail.com>
2026-04-16 15:06:11 +0200

    Initial commit

---
+++ src/main/java/com/eli/nordholm/NordholmApplication.java
@@ -0,0 +1,13 @@
+package com.eli.nordholm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordholmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordholmApplication.class, args);
+    }
+
+}
