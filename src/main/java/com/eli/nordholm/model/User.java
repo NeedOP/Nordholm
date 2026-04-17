4719125aaaf53597452a6a6fee1ab7852ed568bc
maeka <maek.alekdawi@gmail.com>
2026-04-17 12:01:12 +0200

    Setup User entity

---
+++ src/main/java/com/eli/nordholm/model/User.java
@@ -0,0 +1,22 @@
+package com.eli.nordholm.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+    // USER or ADMIN
+    private String role;
+}
---
+++ src/main/java/com/eli/nordholm/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.eli.nordholm.repository;
+
+import com.eli.nordholm.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
