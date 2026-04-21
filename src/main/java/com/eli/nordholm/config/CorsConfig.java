4eaced8979eb1466912d255f3291c737c5edb215
maeka <maek.alekdawi@gmail.com>
2026-04-21 13:41:46 +0200

    Frontend FIX

--- src/main/java/com/eli/nordholm/config/CorsConfig.java
+++ src/main/java/com/eli/nordholm/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://el-kraft-j57vx95to-needops-projects.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
