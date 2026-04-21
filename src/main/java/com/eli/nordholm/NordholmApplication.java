3d62569fdd71efbeb4fe4635e9b279f932f73e55
maeka <maek.alekdawi@gmail.com>
2026-04-21 12:29:00 +0200

    render test

--- src/main/java/com/eli/nordholm/NordholmApplication.java
+++ src/main/java/com/eli/nordholm/NordholmApplication.java
@@ -1,5 +1,6 @@
 package com.eli.nordholm;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,12 @@ public class NordholmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NordholmApplication.class, args);
+    }
+    @PostConstruct
+    public void debug() {
+        System.out.println("=== ENV DEBUG ===");
+        System.out.println("jdbc:postgresql://db.dfjsszmwjlbkhlyqpxzo.supabase.co:5432/postgres?sslmode=require: " + System.getenv("SPRING_DATASOURCE_URL"));
+        System.out.println("postgres: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
     }
 
 }
